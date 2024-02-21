39eed7d02b74626e3e7ffea3617881855d5b49f0
spzSource <sasha.popitich@gmail.com>
2024-02-22 01:12:31 +0200

    Week-4; Assignment-2; Part2

--- week-4/assignment-2/cbf-assignment/src/main/java/org/lenskit/mooc/cbf/WeightedUserProfileBuilder.java
+++ week-4/assignment-2/cbf-assignment/src/main/java/org/lenskit/mooc/cbf/WeightedUserProfileBuilder.java
@@ -25,11 +25,20 @@ public class WeightedUserProfileBuilder implements UserProfileBuilder {
     @Override
     public Map<String, Double> makeUserProfile(@Nonnull List<Rating> ratings) {
         // Create a new vector over tags to accumulate the user profile
-        Map<String,Double> profile = new HashMap<>();
+        Map<String, Double> profile = new HashMap<>();
 
         // TODO Normalize the user's ratings
+        double mean = ratings.stream().map(Rating::getValue).reduce(0.0, Double::sum) / ratings.size();
+
         // TODO Build the user's weighted profile
 
+        for (Rating r : ratings) {
+            Map<String, Double> itemVector = model.getItemVector(r.getItemId());
+            for (Map.Entry<String, Double> entry : itemVector.entrySet()) {
+                double tagScore = (r.getValue() - mean) * entry.getValue();
+                profile.compute(entry.getKey(), (k, v) -> (v == null) ? tagScore : v + tagScore);
+            }
+        }
 
         // The profile is accumulated, return it.
         return profile;
